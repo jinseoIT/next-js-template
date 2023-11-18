3a95649df3f462acd797dfe0d8f61ed2b0f68759
jinseoit <jinseoit@gmail.com>
2023-11-18 20:38:56 +0900

    add: pages 추가

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,5 @@
 import {render, screen} from '@testing-library/react';
 import Home from '@/app/page';
-import '@testing-library/jest-dom'
 
 describe('Home', () => {
   it('renders a heading', () => {
